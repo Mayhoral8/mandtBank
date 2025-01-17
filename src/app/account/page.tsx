f32c236eacf19ffa18d84f8a4fac31697a008938
Mayowa Ayeni <mayhoral008@gmail.com>
2025-01-17 19:27:22 +0100

    add deposit in account

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -64,38 +64,7 @@ const Page = () => {
         </article>
 
         <section className="flex flex-col lg:grid lg:grid-cols-2">
-          <article className="lg:order-2">
-            <article className="grid grid-cols-4 mt-4 lg:mt-0">
-              <div
-                className="flex flex-col items-center cursor-pointer"
-                onClick={handleShowModal}
-              >
-                <BiTransfer className="text-[#2E9175] text-2xl" />
-                <span>Transfer</span>
-              </div>
-              <div
-                className="flex flex-col items-center cursor-pointer"
-                onClick={handleShowModal}
-              >
-                <IoWallet className="text-[#2E9175] text-2xl" />
-                <span>Pay Bills</span>
-              </div>
-              <div className="flex flex-col items-center cursor-pointer">
-                <IoDocumentTextOutline className="text-[#2E9175] text-2xl" />
-                <span>Statement</span>
-              </div>
-              <div className="flex flex-col items-center cursor-pointer">
-                <PiHandDepositFill className="text-[#2E9175] text-2xl" />
-                <span>Deposit</span>
-              </div>
-            </article>
-            <div className="h-64 border rounded-3xl mt-2 bg-[#F8F2F2] w-[90%] mx-auto shadow-md hidden lg:flex lg:flex-col">
-              <div className="bg-[#2E9175] h-[30%] justify-center w-full rounded-t-3xl flex items-center text-center text-white">
-                <span>Scheduled Activity</span>
-              </div>
-              <span className="text-center">No recent activity</span>
-            </div>
-          </article>
+        
 
           <article className="flex flex-col gap-y-4 lg:order-1 mt-6 lg:mt-0">
             <div className="bg-white rounded-xl border shadow-lg w-[90%] mx-auto border-l-4 border-l-[#FFB300] h-28 py-1">
@@ -156,6 +125,39 @@ const Page = () => {
               </div>
             </div>
           </article>
+          <article className="lg:order-2">
+          <article className="grid grid-cols-4 mt-4 lg:mt-0">
+              <div
+                className="flex flex-col items-center cursor-pointer"
+                onClick={handleShowModal}
+              >
+                <BiTransfer className="text-[#2E9175] text-2xl" />
+                <span>Transfer</span>
+              </div>
+              <div
+                className="flex flex-col items-center cursor-pointer"
+                onClick={handleShowModal}
+              >
+                <IoWallet className="text-[#2E9175] text-2xl" />
+                <span>Pay Bills</span>
+              </div>
+              <div className="flex flex-col items-center cursor-pointer">
+                <IoDocumentTextOutline className="text-[#2E9175] text-2xl" />
+                <span>Statement</span>
+              </div>
+              <div className="flex flex-col items-center cursor-pointer">
+                <PiHandDepositFill className="text-[#2E9175] text-2xl" />
+                <span>Deposit</span>
+              </div>
+            </article>
+          <div className="h-64 border rounded-3xl mt-2 bg-[#F8F2F2] w-[90%] mx-auto shadow-md hidden lg:flex lg:flex-col">
+            <div className="bg-[#2E9175] h-[30%] justify-center w-full rounded-t-3xl flex items-center text-center text-white">
+              <span>Scheduled Activity</span>
+            </div>
+            <span className="text-center">No recent activity</span>
+          </div>
+        </article>
+      
         </section>
       </section>
     </main>
